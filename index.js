309b84473b8c998960b7cab9aa8f72fefb677524
Joel De Conto <joel_d.c@hotmail.com>
2023-01-17 17:33:10 -0300

    .

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/email', async  (req, res)=>{
     const emailAddr = 'joel@previsio.com.br';
 
     let body = fs.readFileSync(__dirname+'/emailTemplate.html', 'utf8');
-    let attachment = fs.readFileSync(__dirname+'/abc.pdf').toString("base64");
+    let attachment = fs.readFileSync(__dirname+'/tmp/abc.pdf').toString("base64");
 
     const msg = {
         to: emailAddr,
@@ -44,7 +44,7 @@ app.get('/email', async  (req, res)=>{
     try{
         await sgMail.send(msg);
         console.log('Email enviado com sucesso!');
-        await fs.unlinkSync(__dirname+'/abc.pdf');
+        await fs.unlinkSync(__dirname+'/tmp/abc.pdf');
         console.log('Arquivo deletado');
         res.status(200).json('Email enviado com sucesso!');
     } catch(error){
