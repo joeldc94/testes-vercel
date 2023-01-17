51694c1e202bd8a911d025217c9f596731269c43
Joel De Conto <joel_d.c@hotmail.com>
2023-01-17 17:22:31 -0300

    Envia pdf e deleta arquivo após email

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.get('/email', async  (req, res)=>{
     
     const emailAddr = 'joel@previsio.com.br';
 
-
     let body = fs.readFileSync(__dirname+'/emailTemplate.html', 'utf8');
     let attachment = fs.readFileSync(__dirname+'/abc.pdf').toString("base64");
 
@@ -45,6 +44,8 @@ app.get('/email', async  (req, res)=>{
     try{
         await sgMail.send(msg);
         console.log('Email enviado com sucesso!');
+        await fs.unlinkSync(__dirname+'/abc.pdf');
+        console.log('Arquivo deletado');
         res.status(200).json('Email enviado com sucesso!');
     } catch(error){
         console.log(error);
